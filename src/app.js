15c43e9ca2e3c012b8f0d709845139146d8c2be6
akankshka <44019583+akankshakutal@users.noreply.github.com>
2019-01-16 12:03:53 +0530

    Add landing page
    index.html file is created and read in app.js using readFile
    
    styles needs to be in it's own css file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
+const fs = require("fs");
+
 const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+  fs.readFile("./index.html", "utf8", (err, data) => {
+    res.write(data);
+    res.end();
+  });
 };
 
 // Export a function that can act as a handler
