016987cd8db781b3f1dd363a8ffc3c054ac74483
akankshka <44019583+akankshakutal@users.noreply.github.com>
2019-01-16 14:43:29 +0530

    Handle status code

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
 const fs = require("fs");
 
-const app = (req, res) => {
-  if (req.url == "/") {
-    fs.readFile("./index.html", (err, data) => {
-      res.write(data);
-    });
-    return;
-  }
+const send = function(res, content, statusCode = 200) {
+  res.statusCode = statusCode;
+  res.write(content);
+  res.end();
+};
 
+const app = (req, res) => {
+  let url = req.url;
+  if (req.url == "/") {
+    url = "/index.html";
+  }
   if (req.url === "/favicon.ico") {
     res.end();
     return;
   }
-
-  fs.readFile(`.${req.url}`, (err, data) => {
-    res.write(data);
-    res.end();
+  fs.readFile(`.${url}`, (err, data) => {
+    if (err) {
+      send(res, "Not Found", 404);
+    }
+    send(res, data);
   });
 };
+
 // Export a function that can act as a handler
 
 module.exports = app;
