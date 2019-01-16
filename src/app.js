ef1ee68f29b6a7a8054e247d65d25692c87e0de8
akankshka <44019583+akankshakutal@users.noreply.github.com>
2019-01-16 13:17:44 +0530

    Move style to flowerCatalog.css
    Handle favicon.ico error

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 const fs = require("fs");
 
 const app = (req, res) => {
-  fs.readFile("./index.html", "utf8", (err, data) => {
+  if (req.url == "/") {
+    fs.readFile("./index.html", (err, data) => {
+      res.write(data);
+    });
+    return;
+  }
+
+  if (req.url === "/favicon.ico") {
+    res.end();
+    return;
+  }
+
+  fs.readFile(`.${req.url}`, (err, data) => {
     res.write(data);
     res.end();
   });
 };
-
 // Export a function that can act as a handler
 
 module.exports = app;
